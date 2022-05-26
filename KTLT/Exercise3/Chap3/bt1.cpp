47b6c24955be1fe80d86ae6e6342302efaa782f9
thevu29 <vnguyen132az@gmail.com>
2022-05-26 09:27:35 +0700

    commit

--- KTLT/Exercise3/Chap3/bt1.cpp
+++ KTLT/Exercise3/Chap3/bt1.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include <math.h>
 using namespace std;
 
-void readFile(fstream &f, int a[], int &n)
+void readFile(fstream &fin, int a[], int &n)
 {
-    f.open("inp.txt", ios::in);
-    if(!f)  return;
+    fin.open("inp1.txt", ios::in);
+    if(!fin)  return;
 
     n = 0;
-    while (!f.eof())
-        f >> a[n++];
+    while (!fin.eof())
+        fin >> a[n++];
 }
 
 int Prime(int n)
@@ -58,12 +59,147 @@ int countPalindrome(int a[], int n)
     return cnt;
 }
 
+int GCD(int a, int b)
+{
+    while (a * b != 0)
+    {
+        if (a > b)
+            a %= b;
+        else    
+            b %= a;
+    }
+
+    return a + b;
+}
+
+int countGCD(int a[], int n)
+{
+    int cnt = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (GCD(a[i], a[j]) == 1)
+                cnt++;
+        }
+    }
+
+    return cnt;
+}
+
+int SymetricPrime(int n)
+{
+    int i, j, p, q;
+
+    i = n - 1;
+    while (i >= 2)
+    {
+        if (Prime(i))
+        {
+            p = i;
+            break;
+        }
+        i--;
+    }
+
+    j = n + 1;
+    while (true)
+    {
+        if (Prime(j))
+        {
+            q = j;
+            break;
+        }
+        j++;
+    }
+
+    if (p + q == 2 * n)
+        return 1;
+    return 0;
+}
+
+int countSymetric(int a[], int n)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (Prime(a[i]) && SymetricPrime(a[i]))
+            cnt++;
+    }
+    return cnt;
+}
+
+void deleteSame(int a[], int &n)
+{
+    sort(a, a + n);
+    int j = 0;
+
+    for (int i = 0; i < n; i++)     
+    {
+        if (a[i] != a[j])
+        {
+            j++;
+            a[j] = a[i];
+        }
+    }
+
+    n = j + 1;
+}
+
+int countSame(int a[], int n)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (Prime(a[i]))
+            cnt++;
+    }
+    return cnt;
+}
+
+int Mersenne(int n)
+{
+    int i = 1;
+    while (true)
+    {
+        if (pow(2, i) - 1 == n)
+            return 1;
+        else if (pow(2, i) - 1 > n)
+            return 0;
+        i++;
+    }
+}
+
+int countMersenne(int a[], int n)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (Prime(a[i]) && Mersenne(a[i]))
+            cnt++;
+    }
+    return cnt;
+}
+
+void writeFile(fstream &fout, int a[], int n)
+{
+    fout.open("out1.txt", ios::out);
+    if(!fout)   return;
+
+    fout << countPrime(a, n) << " " << countPalindrome(a, n) << endl;
+    fout << countGCD(a, n) << endl;
+    fout << countSymetric(a, n) << endl;
+    fout << countSame(a, n) << endl;
+    fout << countMersenne(a, n);
+}
+
 int main()
 {
-    fstream f;
+    fstream fin, fout;
     int n, a[100];
-    readFile(f, a, n);
-    cout << countPrime(a, n) << " " << countPalindrome(a, n);
-    f.close();
+    readFile(fin, a, n);
+    writeFile(fout, a, n);
+    fin.close();
     return 0;
 }
